2692cb779058983240e83a3ef502789f37def979
NadetDev <nadetdev@gmail.com>
2024-08-19 18:05:53 +0200

    Init commit

---
+++ src/main/java/me/nadetdev/crudapp/NadetdevCrudDemoAppApplication.java
@@ -0,0 +1,13 @@
+package me.nadetdev.crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadetdevCrudDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NadetdevCrudDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nadetdev/crudapp/NadetdevCrudDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nadetdev.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadetdevCrudDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
